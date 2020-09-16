f81d7c7011615b43376fe609deb2412e53836d8e
Gnlow <entrydark@gmail.com>
2020-09-16 22:55:04 +0900

    write main

---
+++ mod.ts
@@ -0,0 +1,50 @@
+import { Nominal, tuple } from "https://raw.githubusercontent.com/randkid/Randkid/master/mod.ts"
+
+import residence from "https://raw.githubusercontent.com/randkid/residence/master/mod.ts"
+import birthdate from "https://raw.githubusercontent.com/randkid/birthdate/master/mod.ts"
+import cheerio from "https://dev.jspm.io/cheerio"
+import proj4 from "https://dev.jspm.io/proj4"
+
+export default new Nominal({
+    categories: [],
+    inputMaterials: tuple([residence, birthdate]),
+    async rand(seed, residenceVal, birthdateVal) {
+        const age = (new Date).getFullYear() - (new Date(birthdateVal)).getFullYear() + 1
+        const schoolType = age > 16 ? 3 : age > 13 ? 2 : 1
+
+        const form = new URLSearchParams
+        form.append("searchText", residenceVal[1])
+        const data = await fetch("https://schoolzone.emac.kr/gis/totalSearch.do", {
+            method: "POST",
+            body: form
+        }).then(x => x.text())
+
+        const $ = cheerio.load(data)
+
+        const [match, lon, lat] = $("div.result_content_sec:nth-child(3) > div:nth-child(3) > ul:nth-child(3) > li:nth-child(1) > input:nth-child(1)").attr("onclick").match(/parent\.fn_getSchoolArea\((.+),(.+),'.+','vworld'\);/)
+
+        const form2 = new URLSearchParams
+        const epsg5186 = "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=600000 +ellps=GRS80 +units=m +no_defs "
+        const [x, y] = proj4("EPSG:4326", epsg5186, [Number(lon), Number(lat)])
+        form2.append("x", x)
+        form2.append("y", y)
+        form2.append("lon", lon)
+        form2.append("lat", lat)
+        form2.append("schoolType", "elementSchoolArea")
+        const schoolArea = await fetch("https://schoolzone.emac.kr/gis/schoolAreaSearch.do", {
+            method: "POST",
+            body: form2
+        }).then(x => x.text())
+
+        const $2 = cheerio.load(schoolArea)
+        const schools = Array.from($2("tr")).map(
+            ({attribs}: any) => {
+                if (attribs.schoolname && attribs.schooltype == schoolType) {
+                    return [attribs.schoolname, attribs.eduarea, attribs.schooltype]
+                }
+            }
+        ).filter(x => x)
+
+        return schools[Math.floor(Math.random() * schools.length)]
+    }
+})
